15033e17106638ee3ade7c9e9a949e327f836152
Isitoo <isitoo_03@abv.bg>
2025-01-17 16:29:29 +0200

    Small changes

--- WineryApplication/src/main/java/com/oopproject/wineryapplication/Launcher.java
+++ WineryApplication/src/main/java/com/oopproject/wineryapplication/Launcher.java
@@ -17,10 +17,7 @@ public class Launcher extends Application {
         SceneHelper.switchTo(Scenes.WELLCOME);
         stage.setScene(scene);
         stage.show();
-        Parent root = FXMLLoader.load(getClass().getResource("buttons.fxml"));
-        primaryStage.setTitle("Button Controller Example");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        
     }
 
     public static void main(String[] args) {
